535d879db0de46c12cb73d7d336fd21b0a96991e
Ravula Sonali <ravula.sonali@gmail.com>
2023-12-26 19:31:45 +0530

    Assignment-12

---
+++ WebAppPartial/WebAppPartial/Models/Cricket.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System;
+
+namespace WebAppPartial.Models
+{
+
+    public class Cricket
+    {
+        
+        public int TeamId { get; set; }
+        public string TeamName { get; set; }
+        public int NOWC { get; set; }
+    }
+}
